920cde25c4f8ee587586d5c6a13cdc2dd81699e1
Craig Finch <cfinch@ieee.org>
2014-07-24 15:29:55 -0400

    Cleaned up and commented maplotlib examples

--- Craig_Finch_NumPy_SciPy_matplotlib/matplotlib/basic_matplotlib.py
+++ Craig_Finch_NumPy_SciPy_matplotlib/matplotlib/basic_matplotlib.py
@@ -1,11 +1,14 @@
 import numpy as np
-import scipy
 import matplotlib.pyplot as plt
 
+# arange is similar to function range, but returns NumPy array of floats
 x = np.arange(-2 * np.pi, 2 * np.pi, 0.1)
+
+# NumPy functions return NumPy arrays
 cos_x = np.cos(x)
 sin_x = np.sin(x)
 
+# Demonstrate PyPlot interface to matplotlib
 plt.figure()
 
 plt.plot(x, cos_x, label='cos(x)', linewidth=2)
@@ -13,8 +16,9 @@ plt.plot(x, sin_x, label='sin(x)', linewidth=2)
 
 plt.xlabel('x')
 plt.ylabel('y')
+
+# Display the legend, which is automatically built from line labels
 plt.legend()
 
-plt.figure()
-
+# Figure won't appear until we call show()
 plt.show()
--- Craig_Finch_NumPy_SciPy_matplotlib/matplotlib/plot_circles.py
+++ Craig_Finch_NumPy_SciPy_matplotlib/matplotlib/plot_circles.py
@@ -1,26 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_circles(adsorbed_x, adsorbed_y, radius, width, reference_indices=[]):
+def plot_circles(center_x, center_y, radius, width):
+    """Plot circles in a 2D plane.
+
+    Args:
+        center_x: iterable, x coordinates of circle centers
+        center_y: iterable, y coordinates of circle centers
+        radius: float or integer, circle radius
+        width: size of plotting area
+
+    Returns:
+        matplotlib axes with circle patches
+    """
     from matplotlib.patches import Circle
 
-    # Plot each run
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for p in range(len(adsorbed_x)):
-        if len(np.where(reference_indices == p)[0]) > 0:
-            ax.add_patch(Circle((adsorbed_x[p], adsorbed_y[p]), radius, 
-                edgecolor='black', facecolor='black'))
-        else:
-            ax.add_patch(Circle((adsorbed_x[p], adsorbed_y[p]), radius,
-                edgecolor='black', facecolor='blue'))
+    ax = fig.add_subplot(111)  # Get axes object for figure
 
-    ax.set_aspect(1.0)
+    # Add each circle to axes
+    for p in range(len(center_x)):
+        ax.add_patch(Circle((center_x[p], center_y[p]), radius,
+            edgecolor='black', facecolor='blue'))
+
+    ax.set_aspect(1.0)  # Otherwise, a square won't look square
+
+    # Draw borders of plot area
     plt.axhline(y=0, color='k')
     plt.axhline(y=width, color='k')
     plt.axvline(x=0, color='k')
     plt.axvline(x=width, color='k')
-    plt.axis([-0.1*width, width*1.1, -0.1*width, width*1.1])
+
+    plt.axis([-0.1*width, width*1.1, -0.1*width, width*1.1]) # Set view area
     plt.xlabel("x")
     plt.ylabel("y")
 
@@ -32,8 +43,13 @@ domain_size = 20.0
 num_spheres = 15
 
 # INITIALIZE
+
+# Providing a seed ensures that the same sequence of pseudo-random numbers will
+# be generated every time. Good for debugging!
 rng = np.random.RandomState(seed=1)
 
+# Get NumPy arrays of pseudo-random numbers to represent (x,y) coordinates of
+# circle centers
 x = rng.uniform(0, domain_size, num_spheres)
 y = rng.uniform(0, domain_size, num_spheres)
 
